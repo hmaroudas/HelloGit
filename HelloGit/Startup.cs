3c0115b76fabe9774ebfd3cf753670aeee663234
hmaroudas <heraclesmaroudasdev@gmail.com>
2014-04-16 23:54:13 +0100

    Initial HelloGit Push from VS2013.

---
+++ HelloGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloGit.Startup))]
+namespace HelloGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
